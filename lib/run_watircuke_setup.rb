a7ed023ed73784dd9aee26877c782da4ad64c94b
sfistak <sfistak@sfistaks-MacBook-Pro.local>
2011-02-26 12:36:32 +0100

    splitted file for module

---
+++ lib/run_watircuke_setup.rb
@@ -0,0 +1,36 @@
+module RunWatirCukeSetup
+  require 'rubygems'
+  require 'cucumber'
+  require 'colorize'
+
+  require "#{Dir.pwd}/features/support/create_screenshot_folder"
+
+  def finishit(result)
+    puts "Copy the path to browser to view results"
+    puts "#{result}".green
+  end
+
+  def setup(folder)
+    t=Time.now.strftime("%y%m%d%H%M")
+    create_screenshot_folder("#{folder}", t)
+  end                                    
+
+  def validate_browser(bro)
+    browser = %w/ie ff firefox chrome safari/
+    browser.include?(bro)
+  end
+
+  def name_the_test(test)
+    test.gsub(/\w+\_/,"")
+  end
+
+  def default_run
+    puts "Did not found the test.".red
+    puts 
+    puts "You could run me with:"
+    puts "./watircuke.rb @tag output".green
+    puts "or"
+    puts "./watircuke.rb features/my_test.feature output".green
+    puts " "
+  end
+end
--- watircuke.rb
+++ watircuke.rb
@@ -1,82 +1,31 @@
 #!/usr/bin/env ruby
-require 'rubygems'
-require 'cucumber'
-require 'colorize'
+require 'lib/run_watircuke_setup'
+include RunWatirCukeSetup
 
-require "#{Dir.pwd}/features/support/create_screenshot_folder"
+class RunMe        
 
-#this are the tests that have config_ files in config/machine/config_*.yml files - designed to work with sanity checks
-tests=%w/bbc_en bbc_de bbc_pl bbc_es bbc_br bbc_fr staging_en staging_de staging_pl staging_es staging_br staging_fr live_en live_de live_pl live_es live_br live_fr/
-
-class RunMe                       
-  
-  def initialize(tests)
-    @tests = tests
-    # system "killall -9 firefox-bin"   
+  def initialize
+    start(ARGV[0], ARGV[1], ARGV[2])
   end
 
-  def start
-    if @tests.include?(ARGV[0])
-      test_name = name_the_test
-      copy_config_yml_file
-      folder = setup(ARGV[0])           
-      run_with_browser = "BROWSER=#{ARGV[1]}" if validate_browser(ARGV[1])
-      system "cucumber DEF_TEST=TRUE" + "#{run_with_browser}" + " --guess -t @#{test_name} -f html > #{folder}/../#{ARGV[0]}.html"
-      finishit
-    elsif ("#{ARGV[0]}" =~/\@/) 
-      run_with_browser = "BROWSER=#{ARGV[2]}" if validate_browser(ARGV[2])
-      folder = setup(ARGV[1] ||= ARGV[0].gsub("\@","").gsub("\,","_"))
-      system "cucumber " + "#{run_with_browser}" + " --guess -t #{ARGV[0]} -f html > #{folder}/../#{ARGV[1]}.html"
-      finishit
-    elsif ("#{ARGV[0]}" =~/features/) 
-      run_with_browser = "BROWSER=#{ARGV[2]}" if validate_browser(ARGV[2])
-      folder = setup(ARGV[1] ||= ARGV[0].gsub(/\w+\//,"").gsub(/\.\w+/,""))
-      system "cucumber " + "#{run_with_browser}" + " --guess #{ARGV[0]} -f html > #{folder}/../#{ARGV[1]}.html"
-      finishit
+  def start(test, out, browser)
+    if ("#{test}" =~/\@/) 
+      run_with_browser = "BROWSER=#{browser}" if RunWatirCukeSetup::validate_browser(browser)
+      folder = RunWatirCukeSetup::setup(out ||= test.gsub("\@","").gsub("\,","_"))
+      result = "#{folder}/../#{out}.html"
+      system "cucumber " + "#{run_with_browser}" + " --guess -t #{test} -f html > #{result}"
+      RunWatirCukeSetup::finishit(result)
+    elsif ("#{test}" =~/features/) 
+      run_with_browser = "BROWSER=#{browser}" if RunWatirCukeSetup::validate_browser(browser)
+      folder = RunWatirCukeSetup::setup(browser ||= test.gsub(/\w+\//,"").gsub(/\.\w+/,""))
+      system "cucumber " + "#{run_with_browser}" + " --guess #{test} -f html > #{result}"
+      RunWatirCukeSetup::finishit(result)
     else
-      default_run
+      RunWatirCukeSetup::default_run
     end
   end
-      
-  def default_run
-    puts "Did not found the test.".red
-    puts 
-    puts "You could run me with:"
-    puts "./watircuke.rb @tag output".green
-    puts "or"
-    puts "./watircuke.rb features/my_test.feature output".green
-    puts " "
-    @tests.collect! {|x| x + ", " }
-    print "or with any of the tests: "
-    print "#{@tests}".yellow
-    puts
-    puts "./watircuke.rb live_en".green
-  end
-  def finishit
-    puts "Test finished"
-  end
-  
-  def setup(folder)
-    t=Time.now.strftime("%y%m%d%H%M")
-    create_screenshot_folder("#{folder}", t)
-  end                                    
-  
-  def validate_browser(bro)
-    browser = %w/ie ff firefox chrome safari/
-    browser.include?(bro)
-  end
-
-  def name_the_test
-    ARGV[0].gsub(/\w+\_/,"")
-  end
-  
-  def copy_config_yml_file
-    config_name = "config_" + ARGV[0].to_s
-    FileUtils.cp "./features/machines/#{config_name}.yml", "./config/config.yml"
-  end
-
 end
 
-run = RunMe.new(tests)
+RunMe.new
 
-run.start
+
